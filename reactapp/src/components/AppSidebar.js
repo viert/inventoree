551e33079f4ef2e4a8fa5df9a5784e1d5ab6e107
Pavel Vorobyov <aquavitale@yandex.ru>
2017-04-16 11:49:06 +0300

    Sidebar component

---
+++ reactapp/src/components/AppSidebar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+import './AppSidebar.css'
+
+const AppSidebar = () => (
+    <aside className="sidebar">
+        <ul className="sidebar-menu">
+            <li><NavLink to="/datacenters" activeClassName="active">datacenters</NavLink></li>
+            <li><NavLink to="/projects" activeClassName="active">projects</NavLink></li>
+            <li><NavLink to="/groups" activeClassName="active">groups</NavLink></li>
+            <li><NavLink to="/hosts" activeClassName="active">hosts</NavLink></li>
+            <hr/>
+            <li><NavLink to="/structure/users" activeClassName="active">users</NavLink></li>
+        </ul>
+    </aside>
+)
+
+export default AppSidebar
