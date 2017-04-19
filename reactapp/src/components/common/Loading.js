a7e5416026ff16e0b0ad9beda705c48a81396129
Pavel Vorobyov <aquavitale@yandex.ru>
2017-04-19 12:36:29 +0300

    Loading component

---
+++ reactapp/src/components/common/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Loading.css'
+
+const Loading = () => (
+    <div className="loading-block">
+        <div className="loading-block_spinner"></div>
+        <h1>Loading</h1>
+    </div>
+)
+
+export default Loading
