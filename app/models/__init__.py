2b416f78270ccef4291b000409a9d41b67c98d27
Pavel Vorobyov <aquavitale@yandex.ru>
2017-04-20 15:56:50 +0300

    host model started

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from project import Project
 from group import Group
 from datacenter import Datacenter
 from user import User
+from host import Host
--- app/models/host.py
+++ app/models/host.py
@@ -1,4 +1,40 @@
 from storable_model import StorableModel, now
 
 class Host(StorableModel):
-    pass
+
+    FIELDS = (
+        "fqdn",
+        "short_name",
+        "group_id",
+        "datacenter_id",
+        "description",
+        "tags",
+        "created_at",
+        "updated_at",
+    )
+
+    REQUIRED_FIELDS = (
+        "fqdn",
+        "short_name",
+        "group_id",
+    )
+
+    REJECTED_FIELDS = (
+        "created_at",
+        "updated_at",
+    )
+
+    DEFAULTS = {
+        "created_at": now,
+        "updated_at": now
+    }
+
+    INDEXES = (
+        "fqdn",
+        "short_name",
+        ["group_id", "datacenter_id"],
+        "datacenter_id",
+        "tags",
+    )
+
+    __slots__ = FIELDS
