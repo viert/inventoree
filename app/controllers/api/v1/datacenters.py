50c0d3e3a419aa373c507744c0263ae5e7ff28a3
Pavel Vorobyov <aquavitale@yandex.ru>
2017-04-28 23:55:55 +0300

    datacenters api, group api refactored

--- app/controllers/api/v1/datacenters.py
+++ app/controllers/api/v1/datacenters.py
@@ -1,9 +1,21 @@
 from app.controllers.auth_controller import AuthController
-from library.engine.utils import resolve_id, json_response, paginated_data
+from library.engine.utils import resolve_id, json_response, paginated_data, json_exception
 from flask import request
+from bson.objectid import ObjectId
 
 
 datacenters_ctrl = AuthController("datacenters", __name__, require_auth=True)
+
+
+def _get_dc_by_id(dc_id):
+    from app.models import Datacenter
+    dc_id = resolve_id(dc_id)
+    return Datacenter.find_one({
+        "$or": [
+            { "_id": dc_id },
+            { "name": dc_id }
+        ]
+    })
 
 
 @datacenters_ctrl.route("/", methods=["GET"])
@@ -25,3 +37,60 @@ def show(datacenter_id=None):
         ]})
     data = paginated_data(datacenters.sort("name"))
     return json_response(data)
+
+@datacenters_ctrl.route("/", methods=["POST"])
+def create():
+    from app.models import Datacenter
+    dc_attrs = dict([x for x in request.json.items() if x[0] in Datacenter.FIELDS])
+    dc_attrs["parent_id"] = ObjectId(dc_attrs["parent_id"])
+    dc = Datacenter(**dc_attrs)
+    # TODO: check permissions!
+    try:
+        dc.save()
+    except Exception as e:
+        return json_exception(e, 500)
+    return json_response({ "data": dc.to_dict() }, 201)
+
+@datacenters_ctrl.route("/<dc_id>", methods=["PUT"])
+def update(dc_id):
+    dc = _get_dc_by_id(dc_id)
+    if dc is None:
+        return json_response({ "errors": ["Datacenter not found"] }, 404)
+    # TODO: check permissions!
+    try:
+        dc.update(request.json)
+    except Exception as e:
+        return json_exception(e, 500)
+    return json_response({ "data": dc.to_dict() })
+
+@datacenters_ctrl.route("/<dc_id>", methods=["DELETE"])
+def delete(dc_id):
+    dc = _get_dc_by_id(dc_id)
+    if dc is None:
+        return json_response({ "errors": ["Datacenter not found"] }, 404)
+    # TODO: check permissions!
+    try:
+        dc.destroy()
+    except Exception as e:
+        return json_exception(e, 500)
+    return json_response({ "data": dc.to_dict() })
+
+@datacenters_ctrl.route("/<dc_id>/set_parent", methods=["PUT"])
+def set_parent(dc_id):
+    dc = _get_dc_by_id(dc_id)
+    if dc is None:
+        return json_response({ "errors": ["Datacenter not found"] }, 404)
+    # TODO: check permissions!
+    parent_id = request.json.get("parent_id")
+    if dc.parent:
+        try:
+            dc.unset_parent()
+        except Exception as e:
+            return json_exception(e, 500)
+    if parent_id is not None:
+        try:
+            parent_id = resolve_id(parent_id)
+            dc.set_parent(parent_id)
+        except Exception as e:
+            return json_exception(e, 500)
+    return json_response({ "data": dc.to_dict() })
--- app/controllers/api/v1/groups.py
+++ app/controllers/api/v1/groups.py
@@ -1,4 +1,4 @@
-from flask import request, g
+from flask import request
 from bson.objectid import ObjectId
 from app.controllers.auth_controller import AuthController
 from library.engine.utils import resolve_id, json_response, json_exception, paginated_data, diff
@@ -6,10 +6,21 @@ from library.engine.utils import resolve_id, json_response, json_exception, pagi
 groups_ctrl = AuthController("groups", __name__, require_auth=True)
 
 
+def _get_group_by_id(group_id):
+    from app.models import Group
+    group_id = resolve_id(group_id)
+    return Group.find_one({
+        "$or": [
+            { "_id": group_id },
+            { "name": group_id }
+        ]
+    })
+
+
 @groups_ctrl.route("/")
 @groups_ctrl.route("/<group_id>")
 def show(group_id=None):
-    from app.models import Group, Project
+    from app.models import Group
     if group_id is None:
         query = {}
         if "_filter" in request.values:
@@ -29,16 +40,10 @@ def show(group_id=None):
 
 @groups_ctrl.route("/<group_id>", methods=["PUT"])
 def update(group_id):
-    from app.models import Group
-    group_id = resolve_id(group_id)
-    group = Group.find_one({
-        "$or": [
-            { "_id": group_id },
-            { "name": group_id }
-        ]
-    })
+    group = _get_group_by_id(group_id)
     if group is None:
         return json_response({ "errors": ["Group not found"] }, 404)
+    # TODO: check permissions!
     try:
         group.update(request.json)
     except Exception as e:
@@ -47,16 +52,10 @@ def update(group_id):
 
 @groups_ctrl.route("/<group_id>/set_children", methods=["PUT"])
 def set_children(group_id):
-    from app.models import Group
-    group_id = resolve_id(group_id)
-    group = Group.find_one({
-        "$or": [
-            { "_id": group_id },
-            { "name": group_id }
-        ]
-    })
+    group = _get_group_by_id(group_id)
     if group is None:
         return json_response({ "errors": ["Group not found"] }, 404)
+    # TODO: check permissions!
     orig = group.child_ids
     upd = request.json["child_ids"]
     d =  diff(orig, upd)
@@ -81,6 +80,7 @@ def create():
     from app.models import Group
     group_attrs = dict([x for x in request.json.items() if x[0] in Group.FIELDS])
     group_attrs["project_id"] = ObjectId(group_attrs["project_id"])
+    # TODO: check permissions!
     group = Group(**group_attrs)
     try:
         group.save()
@@ -90,16 +90,10 @@ def create():
 
 @groups_ctrl.route("/<group_id>", methods=["DELETE"])
 def delete(group_id):
-    from app.models import Group
-    group_id = resolve_id(group_id)
-    group = Group.find_one({
-        "$or": [
-            { "_id": group_id },
-            { "name": group_id }
-        ]
-    })
+    group = _get_group_by_id(group_id)
     if group is None:
         return json_response({ "errors": ["Group not found"] }, 404)
+    # TODO: check permissions!
     try:
         group.destroy()
     except Exception as e:
