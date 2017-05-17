8f8feba032a84aff928306d396826d1abd561939
Pavel Vorobyov <pavel.vorobyov@lazada.com>
2017-05-17 12:55:34 +0300

    backward compat handlers (temporary) for executer

--- app/controllers/api/v1/open.py
+++ app/controllers/api/v1/open.py
@@ -7,13 +7,16 @@ open_ctrl = AuthController("open", __name__, require_auth=False)
 
 
 @open_ctrl.route("/projects2groups/<project_id>")
-def projects2groups(project_id=None):
+def projects2groups(project_id):
     from app.models import Project
     project_id = resolve_id(project_id)
 
     fields = request.values.get("fields")
     if fields is None:
         fields = ["name"]
+    else:
+        fields = fields.split(',')
+
 
     project = Project.find_one({"$or": [
         { "_id": project_id },
@@ -23,3 +26,59 @@ def projects2groups(project_id=None):
         return json_response({ 'errors': [ "No project found" ]}, 404)
     groups = cursor_to_list(project.groups, fields=fields)
     return json_response(groups)
+
+@open_ctrl.route("/groups/<group_id>")
+def groups(group_id):
+    from app.models import Group
+    group_id = resolve_id(group_id)
+
+    fields = request.values.get("fields")
+    if fields is None:
+        fields = [ "_id", "name", "description", "project_name", "created_at", "updated_at", "parents", "children" ]
+    else:
+        fields = fields.split(',')
+
+    groups = Group.find({ "$or": [
+        { "_id": group_id },
+        { "name": group_id }
+    ]})
+
+    groups = cursor_to_list(groups, fields=fields)
+    for group in groups:
+        group["parents"] = [x.name for x in group["parents"]]
+        group["children"] = [x.name for x in group["children"]]
+    return json_response(groups)
+
+@open_ctrl.route("/groups2hosts/<group_id>")
+def groups2hosts(group_id):
+    from app.models import Group
+    group_id = resolve_id(group_id)
+
+    fields = request.values.get("fields")
+    if fields is None:
+        fields = ["fqdn"]
+    else:
+        fields = fields.split(',')
+    
+    group = Group.find_one({ "$or": [
+        { "_id": group_id },
+        { "name": group_id }
+    ]})
+
+    if group is None:
+        return json_response({ 'errors': [ "No group found" ]}, 404)
+
+    hosts = cursor_to_list(group.hosts, fields=fields)
+    return json_response(hosts)
+
+@open_ctrl.route("/datacenters.json")
+def datacenters():
+    from app.models import Datacenter
+
+    dcs = Datacenter.find({})
+    dcs = cursor_to_list(dcs, fields=["name", "parent", "children", "human_readable"])
+    for dc in dcs:
+        if dc["parent"] is not None:
+            dc["parent"] = dc["parent"].name
+        dc["children"] = [x.name for x in dc["children"]]
+    return json_response(dcs)
