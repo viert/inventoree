b6992c36fe42577c158506919c2087ec4960bb14
Pavel Vorobyov <aquavitale@yandex.ru>
2017-05-21 18:00:35 +0300

    executer_data instead of plenty of strange handlers

--- app/controllers/api/v1/open.py
+++ app/controllers/api/v1/open.py
@@ -1,84 +1,34 @@
 from flask import request
-from bson.objectid import ObjectId, InvalidId
 from app.controllers.auth_controller import AuthController
-from library.engine.utils import resolve_id, json_response, json_exception, cursor_to_list, diff
+from library.engine.utils import json_response, cursor_to_list
 
 open_ctrl = AuthController("open", __name__, require_auth=False)
 
+@open_ctrl.route("/executer_data")
+def executer_data():
+    from app.models import Project, Datacenter, Group, Host
+    if "projects" not in request.values:
+        return json_response({ "errors": [ "'projects' parameter is required for executer_data handler"]}, 400)
+    project_names = request.values["projects"].split(",")
 
-@open_ctrl.route("/projects2groups/<project_id>")
-def projects2groups(project_id):
-    from app.models import Project
-    project_id = resolve_id(project_id)
+    projects = Project.find({ "name": { "$in": project_names }})
+    projects = cursor_to_list(projects)
+    project_ids = [x["_id"] for x in projects]
 
-    fields = request.values.get("fields")
-    if fields is None:
-        fields = ["name"]
-    else:
-        fields = fields.split(',')
+    groups = Group.find({ "project_id": { "$in": project_ids }})
+    groups = cursor_to_list(groups)
+    group_ids = [x["_id"] for x in groups]
 
+    hosts = Host.find({ "group_id": { "$in": group_ids }})
+    hosts = cursor_to_list(hosts)
 
-    project = Project.find_one({"$or": [
-        { "_id": project_id },
-        { "name": project_id }
-    ]})
-    if project is None:
-        return json_response({ 'errors': [ "No project found" ]}, 404)
-    groups = cursor_to_list(project.groups, fields=fields)
-    return json_response(groups)
+    datacenters = Datacenter.find({})
+    datacenters = cursor_to_list(datacenters)
 
-@open_ctrl.route("/groups/<group_id>")
-def groups(group_id):
-    from app.models import Group
-    group_id = resolve_id(group_id)
-
-    fields = request.values.get("fields")
-    if fields is None:
-        fields = [ "_id", "name", "description", "project_name", "created_at", "updated_at", "parents", "children" ]
-    else:
-        fields = fields.split(',')
-
-    groups = Group.find({ "$or": [
-        { "_id": group_id },
-        { "name": group_id }
-    ]})
-
-    groups = cursor_to_list(groups, fields=fields)
-    for group in groups:
-        group["parents"] = [x.name for x in group["parents"]]
-        group["children"] = [x.name for x in group["children"]]
-    return json_response(groups)
-
-@open_ctrl.route("/groups2hosts/<group_id>")
-def groups2hosts(group_id):
-    from app.models import Group
-    group_id = resolve_id(group_id)
-
-    fields = request.values.get("fields")
-    if fields is None:
-        fields = ["fqdn"]
-    else:
-        fields = fields.split(',')
-    
-    group = Group.find_one({ "$or": [
-        { "_id": group_id },
-        { "name": group_id }
-    ]})
-
-    if group is None:
-        return json_response({ 'errors': [ "No group found" ]}, 404)
-
-    hosts = cursor_to_list(group.hosts, fields=fields)
-    return json_response(hosts)
-
-@open_ctrl.route("/datacenters.json")
-def datacenters():
-    from app.models import Datacenter
-
-    dcs = Datacenter.find({})
-    dcs = cursor_to_list(dcs, fields=["name", "parent", "children", "human_readable"])
-    for dc in dcs:
-        if dc["parent"] is not None:
-            dc["parent"] = dc["parent"].name
-        dc["children"] = [x.name for x in dc["children"]]
-    return json_response(dcs)
+    results = {
+        "datacenters": datacenters,
+        "projects": projects,
+        "groups": groups,
+        "hosts": hosts
+    }
+    return json_response({ "data": results })
