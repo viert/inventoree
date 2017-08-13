c449ca699ad7c83162aa2b25dad83954d9fc0e11
pvorobyov <pavel.vorobyov@lazada.com>
2017-08-13 19:11:17 +0300

    Testing environment in tests by default

--- commands/test.py
+++ commands/test.py
@@ -1,12 +1,15 @@
 from commands import Command
 from unittest import main
-from app import tests
 
 class Test(Command):
 
     NO_ARGPARSE = True
 
     def run(self):
+        import os
+        if os.environ.get("CONDUCTOR_ENV") is None:
+            os.environ["CONDUCTOR_ENV"] = "testing"
+        from app import tests
         argv = ['micro.py test'] + self.raw_args
         test_program = main(argv=argv, module=tests, exit=False)
         if test_program.result.wasSuccessful():
