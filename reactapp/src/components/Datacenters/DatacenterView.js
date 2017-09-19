fc42c57edd23453afcdbb584044cd6ca9424d488
pvorobyov <pavel.vorobyov@lazada.com>
2017-09-19 14:45:34 +0300

    preparations for dc and project views

---
+++ reactapp/src/components/Datacenters/DatacenterView.js
@@ -0,0 +1,3 @@
+import React from 'react'
+const DatacenterView = () => <div>Datacenter View</div>
+export default DatacenterView
---
+++ reactapp/src/components/Projects/ProjectView.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react'
+
+const ProjectView = () => <div>Project View</div>
+export default ProjectView
