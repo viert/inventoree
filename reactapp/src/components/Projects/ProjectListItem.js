07aaea4acb5b023414592645903ed9a0b87df039
Pavel Vorobyov <aquavitale@yandex.ru>
2017-09-19 17:50:48 +0300

    unused link removed

--- reactapp/src/components/Projects/ProjectListItem.js
+++ reactapp/src/components/Projects/ProjectListItem.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
 
 import TableModelLink from '../common/TableModelLink'
 import { UserLink } from '../common/UserList'
