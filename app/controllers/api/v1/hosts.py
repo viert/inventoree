720bf10dd3500fe47f05a9f93889d3aa8fa32001
Pavel Vorobyov <aquavitale@yandex.ru>
2017-09-20 17:35:32 +0300

    switched from helper function to storable model get()

--- app/controllers/api/v1/hosts.py
+++ app/controllers/api/v1/hosts.py
@@ -8,17 +8,6 @@ from bson.objectid import ObjectId, InvalidId
 hosts_ctrl = AuthController('hosts', __name__, require_auth=True)
 
 
-def _get_host_by_id(host_id):
-    from app.models import Host
-    host_id = resolve_id(host_id)
-    return Host.find_one({
-        "$or": [
-            { "_id": host_id },
-            { "fqdn": host_id }
-        ]
-    })
-
-
 @hosts_ctrl.route("/", methods=["GET"])
 @hosts_ctrl.route("/<host_id>", methods=["GET"])
 def show(host_id=None):
@@ -92,7 +81,8 @@ def create():
 
 @hosts_ctrl.route("/<host_id>", methods=["PUT"])
 def update(host_id):
-    host = _get_host_by_id(host_id)
+    from app.models import Host
+    host = Host.get(host_id)
     if host is None:
         return json_response({ "errors": [ "Host not found" ] }, 404)
 
@@ -131,7 +121,8 @@ def update(host_id):
 
 @hosts_ctrl.route("/<host_id>", methods=["DELETE"])
 def delete(host_id):
-    host = _get_host_by_id(host_id)
+    from app.models import Host
+    host = Host.get(host_id)
 
     if host is None:
         return json_response({ "errors": [ "Host not found" ] }, 404)
