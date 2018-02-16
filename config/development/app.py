24177c67a7c8daee2f713b308533a583c4bbbc68
Pavel Vorobyov <p.vorobyov@corp.mail.ru>
2018-02-16 15:25:34 +0300

    session cookie renamed

--- config/development/app.py
+++ config/development/app.py
@@ -1,7 +1,9 @@
 CSRF_PROTECTION = False
 DOCUMENTS_PER_PAGE = 20
 SECRET_KEY = "some_secret_key"
-FLASK_APP_SETTINGS = {}
+FLASK_APP_SETTINGS = {
+    "SESSION_COOKIE_NAME": "conductor_sid"
+}
 PORT = 3000
 STATIC_FOLDER = "static"
 AUTHORIZER = "SysAuthorizer"
