0b6a746ee1ef74428082b49626d0a426c113a24b
Pavel Vorobyov <p.vorobyov@corp.mail.ru>
2018-03-19 17:14:10 +0300

    datacenters http api tests

--- app/tests/__init__.py
+++ app/tests/__init__.py
@@ -8,6 +8,7 @@ from app.tests.httpapi.test_account_ctrl import TestAccountCtrl
 from app.tests.httpapi.test_group_ctrl import TestGroupCtrl
 from app.tests.httpapi.test_host_ctrl import TestHostCtrl
 from app.tests.httpapi.test_user_ctrl import TestUserCtrl
+from app.tests.httpapi.test_datacenter_ctrl import TestDatacenterCtrl
 
 from app.tests.utils.test_pbkdf2 import TestPBKDF2
 from app.tests.utils.test_diff import TestDiff
