3e02da47171bad7ee1f7d3c741ae4a341eb1bd36
Pavel Vorobyov <p.vorobyov@corp.mail.ru>
2018-04-03 16:26:52 +0300

    version up

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from library.engine.baseapp import BaseApp
 
 
 class App(BaseApp):
-    VERSION = "6.8.2"
+    VERSION = "6.8.3"
 
     def configure_routes(self):
         self.logger.info("Configuring conductor endpoints")
