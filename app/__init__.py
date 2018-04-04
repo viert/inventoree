7d5eb531d9c6985ea2fdbdbe284a3d149b550aec
Pavel Vorobyov <p.vorobyov@corp.mail.ru>
2018-04-04 12:41:56 +0300

    Renaming

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from library.engine.baseapp import BaseApp
 
 
 class App(BaseApp):
-    VERSION = "6.8.4"
+    VERSION = "6.8.5"
 
     def configure_routes(self):
         self.logger.info("Configuring conductor endpoints")
