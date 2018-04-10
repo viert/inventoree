2452bb3fa45c62232bcd32789657259fd24e192e
Pavel Vorobyov <p.vorobyov@corp.mail.ru>
2018-04-10 13:48:58 +0300

    hosts actions

--- app/models/api_action.py
+++ app/models/api_action.py
@@ -1,5 +1,6 @@
 from storable_model import StorableModel, now
 from library.engine.utils import resolve_id
+from library.engine.permutation import expand_pattern
 
 
 class ApiAction(StorableModel):
@@ -54,6 +55,42 @@ class ApiAction(StorableModel):
         if self._id is None:
             self._set_computed()
 
+    def _compute_host_create(self):
+        from app.models import Group, Datacenter
+        host_fqdns = []
+        fqdn_pattern = ""
+        group_name = ""
+        datacenter_name = ""
+        if "fqdn" in self.params:
+            host_fqdns = [self.params["fqdn"]]
+        elif "fqdn_pattern" in self.params:
+            fqdn_pattern = self.params["fqdn_pattern"]
+            host_fqdns = list(expand_pattern(fqdn_pattern))
+        if "group_id" in self.params:
+            group = Group.get(self.params["group_id"])
+            if group is not None:
+                group_name = group.name
+        if "datacenter_id" in self.params:
+            dc = Datacenter.get(self.params["datacenter_id"])
+            if dc is not None:
+                datacenter_name = dc.name
+        self.computed = {
+            "host_fqdns": host_fqdns,
+            "fqdn_pattern": fqdn_pattern,
+            "group_name": group_name,
+            "datacenter_name": datacenter_name
+        }
+
+    def _compute_host_delete(self):
+        from app.models import Host
+        host_fqdn = ""
+        host = Host.get(self.kwargs["host_id"])
+        if host is not None:
+            host_fqdn = host.fqdn
+        self.computed = {
+            "host_fqdn": host_fqdn
+        }
+
     def _compute_host_update(self):
         from app.models import Host, Group, Datacenter
         host_data = {}
@@ -87,11 +124,9 @@ class ApiAction(StorableModel):
 
     def _compute_host_mass_move(self):
         from app.models import Host, Group
-        host_fqdns = []
         group_name = ""
-        if "host_ids" in self.params:
-            hosts = Host.find({"_id": {"$in": [resolve_id(x) for x in self.params.get("host_ids", [])]}})
-            host_fqdns = [x.fqdn for x in hosts]
+        hosts = Host.find({"_id": {"$in": [resolve_id(x) for x in self.params.get("host_ids", [])]}})
+        host_fqdns = [x.fqdn for x in hosts]
         if "group_id" in self.params:
             group = Group.get(self.params.get("group_id", None))
             if group is not None:
@@ -101,13 +136,22 @@ class ApiAction(StorableModel):
             "group_name": group_name
         }
 
+    def _compute_host_mass_delete(self):
+        from app.models import Host
+        hosts = Host.find({"_id": {"$in": [resolve_id(x) for x in self.params.get("host_ids", [])]}})
+        host_fqdns = [x.fqdn for x in hosts]
+        self.computed = {
+            "host_fqdns": host_fqdns
+        }
+
+    def _compute_host_mass_detach(self):
+        self._compute_host_mass_delete()
+
     def _compute_host_mass_set_datacenter(self):
         from app.models import Host, Datacenter
-        host_fqdns = []
         datacenter_name = ""
-        if "host_ids" in self.params:
-            hosts = Host.find({"_id": {"$in": [resolve_id(x) for x in self.params.get("host_ids", [])]}})
-            host_fqdns = [x.fqdn for x in hosts]
+        hosts = Host.find({"_id": {"$in": [resolve_id(x) for x in self.params.get("host_ids", [])]}})
+        host_fqdns = [x.fqdn for x in hosts]
         if "datacenter_id" in self.params:
             dc = Datacenter.get(self.params.get("datacenter_id", None))
             if dc is not None:
@@ -132,6 +176,88 @@ class ApiAction(StorableModel):
             "group_name": group_name,
             "project_name": project_name
         }
+
+    def _compute_group_delete(self):
+        from app.models import Group
+        group_name = ""
+        group = Group.get(self.kwargs["dc_id"])
+        if group is not None:
+            group_name = group.name
+        self.computed = {
+            "group_name": group_name
+        }
+
+    def _compute_group_mass_delete(self):
+        from app.models import Group
+        groups = Group.find({"_id": {"$in": [resolve_id(x) for x in self.params.get("group_ids", [])]}})
+        group_names = [x.fqdn for x in groups]
+        self.computed = {
+            "group_names": group_names
+        }
+
+    def _compute_group_mass_move(self):
+        from app.models import Group, Project
+        groups = Group.find({"_id": {"$in": [resolve_id(x) for x in self.params.get("group_ids", [])]}})
+        group_names = [x.fqdn for x in groups]
+        project_name = ""
+        if "project_id" in self.params:
+            project = Project.get(self.params["project_id"])
+            if project is not None:
+                project_name = project.name
+        self.computed = {
+            "group_names": group_names,
+            "project_name": project_name
+        }
+
+    def _compute_group_set_children(self):
+        from app.models import Group
+        group_name = ""
+        children = Group.find({"_id": {"$in": [resolve_id(x) for x in self.params.get("child_ids", [])]}})
+        child_group_names = [x.name for x in children]
+        group = Group.get(self.kwargs["group_id"])
+        if group is not None:
+            group_name = group.name
+        self.computed = {
+            "group_name": group_name,
+            "child_group_names": child_group_names
+        }
+
+    def _compute_group_set_hosts(self):
+        from app.models import Group, Host
+        group_name = ""
+        hosts = Host.find({"_id": {"$in": [resolve_id(x) for x in self.params.get("host_ids", [])]}})
+        host_fqdns = [x.fqdn for x in hosts]
+        group = Group.get(self.kwargs["group_id"])
+        if group is not None:
+            group_name = group.name
+        self.computed = {
+            "group_name": group_name,
+            "host_fqdns": host_fqdns
+        }
+
+    def _compute_group_update(self):
+        from app.models import Group, Project
+        group_name = ""
+        group_data = {}
+        group = Group.get(self.kwargs["group_id"])
+        if group is not None:
+            group_name = group.name
+            for k, v in self.params.iteritems():
+                if k in Group.FIELDS:
+                    old_value = getattr(group, k)
+                    if v != old_value and str(v) != str(old_value):
+                        group_data[k] = v
+            if "project_id" in group_data:
+                project_name = ""
+                project = Project.get(group_data["project_id"])
+                if project is not None:
+                    project_name = project.name
+                group_data["project_name"] = project_name
+        self.computed = {
+            "group_name": group_name,
+            "group_data": group_data
+        }
+
 
     def _compute_datacenter_create(self):
         from app.models import Datacenter
