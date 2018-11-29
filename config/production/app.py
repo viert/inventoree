f9a4e4df7747c1dc1663a55a4c89b645f012c5a9
Pavel Vorobyov <p.vorobyov@corp.mail.ru>
2018-11-29 10:49:06 +0300

    Update app.py

--- config/production/app.py
+++ config/production/app.py
@@ -8,9 +8,4 @@ PORT = 3000
 STATIC_FOLDER = "static"
 AUTHORIZER = "LocalAuthorizer"
 ACTION_LOGGING = True
-
-<<<<<<< HEAD
-=======
-GRAVATAR_PATH = "https://sys.mail.ru/avatar/internal"
 DEFAULT_GROUP_POSTFIX = "_unknown"
->>>>>>> be5554b4efe2a68d08816aee6ba42430a6fbc7a5
